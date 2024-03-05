6e5f46ecc4af441d97825d679a2188c6728f16c2
mschen-sta <cherrie.chen@ycdsb.ca>
2024-03-05 08:35:26 -0500

    Created Lesson.py

---
+++ Lesson.py
@@ -0,0 +1,14 @@
+# EXAMPLE #1:
+# Output Hello World!
+
+# EXAMPLE #2:
+# This is a comment. The computer will not read this line.
+# print("This is a comment even though it looks like a print statement. It will not run because it starts with a #")
+
+# EXAMPLE #3:
+'''
+Anything between the triple quotes,
+will not be run.
+The computer will ignore
+all of these lines.
+'''
