e07806e743aaab41bea934f30a41ef479a2c97e3
mschen-sta <cherrie.chen@ycdsb.ca>
2024-03-04 21:57:54 -0500

    Updated examples

--- Program1.py
+++ Program1.py
@@ -1,1 +1,28 @@
+# EXAMPLE #1:
+# Type: print("Hello World!") below
 
+
+# EXAMPLE #2:
+# Try outputting your own line using the print statement
+
+
+# EXAMPLE #3:
+# Try printing multiple lines of text
+
+
+# EXAMPLE #4:
+# Take your print statement from example #2 and make it into a comment
+
+
+# EXAMPLE #5:
+# Write a multi-line comment
+
+
+# EXAMPLE #6:
+# Recreate the text image below using print statements! (or create your own - look up ASCII Art)
+'''
+  /\_/\
+ (='.'=)
+(")___(")
+ (_____)~~
+'''
