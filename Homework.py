1e49a7b95c5ca6a7cc0106960889642c90a504d8
mschen-sta <cherrie.chen@ycdsb.ca>
2024-03-05 08:46:03 -0500

    Updated Homework.py

--- Homework.py
+++ Homework.py
@@ -1,24 +1,24 @@
-# EXAMPLE #1:
-# Type: print("Hello World!") below
+# QUESTION #1:
+# Type: print("Hello World!") below. Make sure to run your program to see if it works!
 
 
-# EXAMPLE #2:
+# QUESTION #2:
 # Try outputting your own line using the print statement
 
 
-# EXAMPLE #3:
+# QUESTION #3:
 # Try printing multiple lines of text
 
 
-# EXAMPLE #4:
+# QUESTION #4:
 # Take your print statement from example #2 and make it into a comment
 
 
-# EXAMPLE #5:
+# QUESTION #5:
 # Write a multi-line comment
 
 
-# EXAMPLE #6:
+# QUESTION #6:
 # Recreate the text image below using print statements! (or create your own - look up ASCII Art)
 '''
   /\_/\
